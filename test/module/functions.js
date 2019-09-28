7efe73c4b6c5b704f2605b913373fd68b855871a
Jack Works <zjwpeter@gmail.com>
2019-09-28 11:11:13 +0800

    test: add test for new behavior of inner Function constructor

--- test/module/functions.js
+++ test/module/functions.js
@@ -1,29 +1,70 @@
 import test from 'tape';
 import { repairFunctions } from '../../src/repair/functions';
+import Realm from '../../src/realm';
 
 test('repairFunctions', specs => {
   repairFunctions();
 
   specs.test('Function.prototype.constructor', t => {
-    t.plan(4);
+    t.plan(7);
 
     // eslint-disable-next-line no-new-func
     t.doesNotThrow(() => Function(''));
 
     // eslint-disable-next-line no-proto
-    t.throws(() => Error.__proto__.constructor(''), TypeError);
-    t.throws(() => Function.prototype.constructor(''), TypeError);
+    t.doesNotThrow(() => Error.__proto__.constructor(''));
+    t.doesNotThrow(() => Function.prototype.constructor(''));
 
     const proto = Object.getPrototypeOf((0, eval)('(function() {})'));
-    t.throws(() => proto.constructor(''), TypeError);
+    t.doesNotThrow(() => proto.constructor(''));
+
+    const realms = Realm.makeCompartment();
+
+    // eslint-disable-next-line no-proto
+    t.throws(
+      () => realms.evaluate("Error.__proto__.constructor('')"),
+      TypeError
+    );
+    t.throws(
+      () => realms.evaluate("Function.prototype.constructor('')"),
+      TypeError
+    );
+
+    t.throws(
+      () =>
+        realms.evaluate(`
+          const proto = Object.getPrototypeOf((0, eval)('(function() {})'));
+          proto.constructor('')`),
+      TypeError
+    );
   });
 
   specs.test('AsyncFunction.constructor', t => {
-    t.plan(1);
+    t.plan(2);
 
     try {
       const proto = Object.getPrototypeOf((0, eval)('(async function() {})'));
-      t.throws(() => proto.constructor(''), TypeError);
+      t.doesNotThrow(() => proto.constructor(''));
+    } catch (e) {
+      if (
+        e instanceof SyntaxError &&
+        e.message.startsWith('Unexpected token')
+      ) {
+        t.pass('not supported');
+      } else {
+        throw e;
+      }
+    }
+
+    try {
+      const realms = Realm.makeCompartment();
+      t.throws(
+        () =>
+          realms.evaluate(`
+            const proto = Object.getPrototypeOf((0, eval)('(async function() {})'));
+            proto.constructor('')`),
+        TypeError
+      );
     } catch (e) {
       if (
         e instanceof SyntaxError &&
@@ -37,11 +78,31 @@ test('repairFunctions', specs => {
   });
 
   specs.test('GeneratorFunction.constructor', t => {
-    t.plan(1);
+    t.plan(2);
 
     try {
       const proto = Object.getPrototypeOf((0, eval)('(function* () {})'));
-      t.throws(() => proto.constructor(''), TypeError);
+      t.doesNotThrow(() => proto.constructor(''));
+    } catch (e) {
+      if (
+        e instanceof SyntaxError &&
+        e.message.startsWith('Unexpected token')
+      ) {
+        t.pass('not supported');
+      } else {
+        throw e;
+      }
+    }
+    try {
+      const realm = Realm.makeCompartment();
+      t.throws(() =>
+        realm.evaluate(
+          `
+        const proto = Object.getPrototypeOf((0, eval)('(function* () {})'));
+        proto.constructor('');`,
+          TypeError
+        )
+      );
     } catch (e) {
       if (
         e instanceof SyntaxError &&
@@ -55,11 +116,31 @@ test('repairFunctions', specs => {
   });
 
   specs.test('AsyncGeneratorFunction.constructor', t => {
-    t.plan(1);
+    t.plan(2);
 
     try {
       const proto = Object.getPrototypeOf((0, eval)('(async function* () {})'));
-      t.throws(() => proto.constructor(''), TypeError);
+      t.doesNotThrow(() => proto.constructor(''));
+    } catch (e) {
+      if (
+        e instanceof SyntaxError &&
+        e.message.startsWith('Unexpected token')
+      ) {
+        t.pass('not supported');
+      } else {
+        throw e;
+      }
+    }
+    try {
+      const realm = Realm.makeCompartment();
+      t.throws(
+        () =>
+          realm.evaluate(`
+        const proto = Object.getPrototypeOf((0, eval)('(async function* () {})'));
+        proto.constructor('');
+      `),
+        TypeError
+      );
     } catch (e) {
       if (
         e instanceof SyntaxError &&
