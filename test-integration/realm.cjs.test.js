2d9cd9870d6d2b0fb0f1dcec809afac3b1130f24
JF Paradis <jfparadis@users.noreply.github.com>
2019-10-01 08:54:43 -0700

    Add integration tests

---
+++ test-integration/realm.cjs.test.js
@@ -0,0 +1,29 @@
+import test from 'tape';
+
+const Realm = require('../dist/realms-shim.cjs.js');
+
+test('new Realm', t => {
+  t.plan(1);
+
+  t.throws(() => new Realm(), TypeError, 'new Real() should throws');
+});
+
+test('Realm.makeRootRealm()', t => {
+  t.plan(3);
+
+  const r = Realm.makeRootRealm();
+
+  t.ok(r instanceof Realm);
+  t.ok(r.global instanceof r.global.Object);
+  t.equal(r.evaluate('42'), 42);
+});
+
+test('Realm.makeCompartment()', t => {
+  t.plan(3);
+
+  const r = Realm.makeCompartment();
+
+  t.ok(r instanceof Realm);
+  t.ok(r.global instanceof Object);
+  t.equal(r.evaluate('42'), 42);
+});
---
+++ test-integration/realm.esm.test.js
@@ -0,0 +1,28 @@
+import test from 'tape';
+import Realm from '../dist/realms-shim.esm';
+
+test('new Realm', t => {
+  t.plan(1);
+
+  t.throws(() => new Realm(), TypeError, 'new Real() should throws');
+});
+
+test('Realm.makeRootRealm()', t => {
+  t.plan(3);
+
+  const r = Realm.makeRootRealm();
+
+  t.ok(r instanceof Realm);
+  t.ok(r.global instanceof r.global.Object);
+  t.equal(r.evaluate('42'), 42);
+});
+
+test('Realm.makeCompartment()', t => {
+  t.plan(3);
+
+  const r = Realm.makeCompartment();
+
+  t.ok(r instanceof Realm);
+  t.ok(r.global instanceof Object);
+  t.equal(r.evaluate('42'), 42);
+});
